743f618e80fa14a2e4cbfad881c9f2be64d1c32f
Tejas Joshi <tejas.joshi@tantalumcorporation.com>
2019-02-13 16:28:54 +0530

    done with basic UI and validation

--- app/src/main/java/com/bca/tj/loginwithsocialsites/RegisterActivity.kt
+++ app/src/main/java/com/bca/tj/loginwithsocialsites/RegisterActivity.kt
@@ -1,21 +1,91 @@
 package com.bca.tj.loginwithsocialsites
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
 import android.text.TextUtils
-import com.google.firebase.auth.FirebaseAuth
+import android.text.TextWatcher
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var firebaseAuth: FirebaseAuth
+    //private lateinit var fireBaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        initUI()
         buttonRegister.setOnClickListener {
-          if(TextUtils.isEmpty(etEmail.text))
-              editTextEmail.error = "Empty Email!!!"
-
+            validateEmailAndPassword()
         }
     }
+
+    private fun validateEmailAndPassword() {
+        when {
+            TextUtils.isEmpty(etEmail.text) -> {
+                editTextEmail.isErrorEnabled = true
+                editTextEmail.error = "Empty Email!!!"
+            }
+            TextUtils.isEmpty(etPassword.text) -> {
+                etPasswordLayout.isErrorEnabled = true
+                etPasswordLayout.error = "Empty Password!!"
+            }
+        }
+    }
+
+    private fun initUI() {
+        etEmail.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                isTextEmptyOrNot(p0.toString())
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                isTextEmptyOrNot(p0.toString())
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                isTextEmptyOrNot(p0.toString())
+            }
+
+        })
+
+        etPassword.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                isPasswordEmptyOrNot(p0.toString())
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                isPasswordEmptyOrNot(p0.toString())
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                isPasswordEmptyOrNot(p0.toString())
+            }
+
+        })
+    }
+
+
+    private fun isTextEmptyOrNot(p0: String?) {
+        if (!(p0.isNullOrBlank()))
+            editTextEmail.isErrorEnabled = false
+        else {
+            editTextEmail.isErrorEnabled = true
+            editTextEmail.error = "Empty Email!!!"
+        }
+    }
+
+    private fun isPasswordEmptyOrNot(p0: String?) {
+        when{
+            p0.isNullOrBlank() -> {}
+        }
+
+        if (!(p0.isNullOrBlank()))
+            etPasswordLayout.isErrorEnabled = false
+        else {
+            etPasswordLayout.isErrorEnabled = true
+            etPasswordLayout.error = "Empty Password!!!"
+        }
+    }
+
+
 }
